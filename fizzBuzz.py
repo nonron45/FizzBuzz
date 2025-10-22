da1a58d3ac1850d4fd56ed4ee152c92fc5a2d2f4
sad-aou <nonron4545@gmail.com>
2025-10-22 13:52:52 +0100

    test n=0,1,2

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -3,3 +3,5 @@ def fizzBuzz (n:int)-> int:
           return 0
     if n==1:
         return 1
+    if n==2:
+        return 2
--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -1,6 +1,6 @@
 from  fizzBuzz import fizzBuzz
 import pytest
-@pytest.mark.parametrize("n, expected_result",[(0,0),(1,1)])
+@pytest.mark.parametrize("n, expected_result",[(0,0),(1,1),(2,2)])
 
 def test_fizzBuzz0return1 (n, expected_result):
     result:int  = fizzBuzz(n)
