63cd9114ca29f356b5c570589f2358ac8e060a09
sad-aou <nonron4545@gmail.com>
2025-10-22 13:29:35 +0100

    first test n=0

--- test_fizzBuzz.py
+++ test_fizzBuzz.py
@@ -1,7 +1,7 @@
 import pytest
 def fizzBuzz (n:int)-> int:
     if n==0:
-          return 1
+          return 0
 def test_fizzBuzz0return1 ():
     result = fizzBuzz(0)
-    assert  result ==1
+    assert  result ==0
